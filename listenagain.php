77f6897c8119599a2d6cff89dea67ed2632dad2e
Nick Piggott <nick@piggott.eu>
2022-09-25 11:27:26 +0100

    Tidying up and consistency, and message if no listen again is found

--- listenagain.php
+++ listenagain.php
@@ -49,41 +49,52 @@ function listenagain_catalogue($rss_url) {
 	usort($shows, function ($a, $b) {
 		return $a['pub_date'] <= $b['pub_date'];
 	});
+	
+	$unique_shows = array_filter($shows, function ($a) use($attrs) {
+ 		return (strcmp($a['title'], $attrs['title']) == 0);
+	});
 
-	return $shows; 
+	return $unique_shows; 
 }
 
 // implements the [listenagain title="title"] shortcode
 function listenagain_shortcode($attrs) {
 
-	$shows = listenagain_catalogue('https://podcast.canstream.co.uk/{your station name here}/audio/rss.xml');
-	$unique_shows = array_reverse(array_values(array_column(
-		array_reverse($shows),
-		null,
-		'title'
-	)));
+	$shows = listenagain_catalogue('https://podcast.canstream.co.uk/{your station name here}/audio/rss.xml',$attrs);
 
-        $show = $unique_shows[array_search($attrs['title'],array_column($unique_shows,'title'))];
-	return '<div id="listen-again-'.str_replace(' ', '', $show['title']).'" class="listen-again-embed">'.$show['title'].'</div><div class="listen-again-date">'.date_format($show['pub_date'],'l dS F, H:i').'</div><div class="listen-again-player"><a href="'.$show['url'].'">PLAYER</a></div>';
+//	$unique_shows = array_reverse(array_values(array_column(
+//      array_reverse($shows),
+//		null,
+//		'title'
+//	)));
+
+//        $show = $unique_shows[array_search($attrs['title'],array_column($unique_shows,'title'))];
+        
+    if (count($shows) === 0) {
+        $html = 'There is no listen again for '.$attrs['title'];
+        } else {
+        $html = '<div id="listen-again-'.str_replace(' ', '', $shows['title']).'" class="listen-again-embed">'.$shows['title'].'</div><div class="listen-again-date">'.date_format($shows['pub_date'],'l dS F, H:i').'</div><div class="listen-again-player"><a href="'.$shows['url'].'">PLAYER</a></div>';
+     }
+         
+return $html;
 
 }
 
 // implements the [listenagain_all title="title"] shortcode
 function listenagain_all_shortcode($attrs) {
 
-	$shows = listenagain_catalogue('https://podcast.canstream.co.uk/{your station name here}/audio/rss.xml');
-	$unique_shows = array_filter($shows, function ($a) use($attrs) {
- 		return (strcmp($a['title'], $attrs['title']) == 0);
-	});
-
-
-	$html = '';
-	foreach ($unique_shows as $show) {
-		$html = $html . '<div id="listen-again-'.str_replace(' ', '', $show['title']).'" class="listen-again-embed">'.$show['title'].'</div><div class="listen-again-date">'.date_format($show['pub_date'],'l dS F, H:i').'</div><div class="listen-again-player"><a href="'.$show['url'].'">PLAYER</a></div>';
-	}
+	$shows = listenagain_catalogue('https://podcast.canstream.co.uk/{your station name here}/audio/rss.xml',$attrs);
 	
-	if ($html = '') $html = '<div id="listen-again-empty" class="listen-again-embed">There aren\'t any listen again shows for ' . $attrs['title'].'</div>';
- return $html;
+	if (count($shows) === 0) {
+            $html = '<div id="listen-again-empty" class="listen-again-embed">There aren\'t any listen again shows for ' . $attrs['title'].'</div>';
+       		} else {
+				$html = ''       			
+				foreach ($unique_shows as $show) {
+					$html = $html . '<div id="listen-again-'.str_replace(' ', '', $show['title']).'" class="listen-again-embed">'.$show['title'].'</div><div class="listen-again-date">'.date_format($show['pub_date'],'l dS F, H:i').'</div><div class="listen-again-player"><a href="'.$show['url'].'">PLAYER</a></div>';
+					}		
+				}
+				
+return $html;
 
 }
 
